ca48b7ae10c408956f0cee665961e726c80579cb
thanakorn2304bd <thanakorn2304bd@gmail.com>
2026-04-03 20:37:04 +0700

    TIDB

--- config/database.php
+++ config/database.php
@@ -2,6 +2,13 @@
 
 use Illuminate\Support\Str;
 
+$tidbHost = env('TIDB_HOST');
+$tidbPort = env('TIDB_PORT');
+$tidbDatabase = env('TIDB_DATABASE');
+$tidbUser = env('TIDB_USER');
+$tidbPassword = env('TIDB_PASSWORD');
+$mysqlSslCa = env('MYSQL_ATTR_SSL_CA', $tidbHost ? '/etc/ssl/cert.pem' : null);
+
 return [
 
     /*
@@ -16,7 +23,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'sqlite'),
+    'default' => env('DB_CONNECTION', $tidbHost ? 'mysql' : 'sqlite'),
 
     /*
     |--------------------------------------------------------------------------
@@ -46,11 +53,11 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DB_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'laravel'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => env('DB_HOST', $tidbHost ?? '127.0.0.1'),
+            'port' => env('DB_PORT', $tidbPort ?? '3306'),
+            'database' => env('DB_DATABASE', $tidbDatabase ?? 'laravel'),
+            'username' => env('DB_USERNAME', $tidbUser ?? 'root'),
+            'password' => env('DB_PASSWORD', $tidbPassword ?? ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => env('DB_CHARSET', 'utf8mb4'),
             'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
@@ -59,7 +66,7 @@ return [
             'strict' => true,
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
-                (PHP_VERSION_ID >= 80500 ? \Pdo\Mysql::ATTR_SSL_CA : \PDO::MYSQL_ATTR_SSL_CA) => env('MYSQL_ATTR_SSL_CA'),
+                (PHP_VERSION_ID >= 80500 ? \Pdo\Mysql::ATTR_SSL_CA : \PDO::MYSQL_ATTR_SSL_CA) => $mysqlSslCa,
             ]) : [],
         ],
 
